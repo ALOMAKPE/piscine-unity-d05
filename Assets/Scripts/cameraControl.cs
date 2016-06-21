ba00924e350a7176d3698ef4b598b7f519ef37e9
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2016-06-21 23:36:53 +0200

    Golfing

--- Assets/Scripts/cameraControl.cs
+++ Assets/Scripts/cameraControl.cs
@@ -1,49 +1,101 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class cameraControl : MonoBehaviour {
 
+	public static cameraControl instance { get; private set; }
+	
 	public Camera cam { get; private set; }
 	public Rigidbody body { get; private set; }
 
-	public bool withMouse = true;
+	public bool golfMode { get; private set; }
 	public float mouseRatio = 0.5f;
-	public bool withKeys = true;
 	public float speed = 50f;
-
+	
 	private Vector3 lastMousePosition;
 	private float cameraX;
 	private float cameraY;
+
+	private Quaternion fireDirection;
+	private bool fireDirectionSet = false;
+
+	private float firePower = 0f;
+	public Vector3 clubPower = new Vector3 (0, 5, 10);
+
+	public RectTransform powerMeter;
+
+	void Awake () {
+		if (!instance)
+			instance = this;
+	}
 
 	void Start () {
 		cam = GetComponent<Camera> ();
 		body = GetComponent<Rigidbody> ();
 		lastMousePosition = Input.mousePosition;
+		golfMode = true;
 		cameraReset ();
 	}
 	
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-			withMouse = withMouse ? false : true;
+		if ((!golfMode && Input.GetKeyDown(KeyCode.Space)) || Input.GetKeyDown (KeyCode.Escape) || Input.GetKeyDown (KeyCode.F)) {
+			golfMode = true;
+			// NOT YET FIRING
+			firePower = 0f;
 			cameraReset();
+		} else if (golfMode) {
+			if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Q)) {
+				golfMode = false;
+			}
 		}
-		if (withMouse) {
+		if (!golfMode) {
+			// ARROW
+			golfBall.instance.arrow.SetActive(false);
+			// CURSOR
 			Cursor.lockState = CursorLockMode.Confined;
 			Cursor.visible = false;
-//			Camera.main.transform.rotation *= (Vector3)(Input.mousePosition - lastPosition) * mouseRatio;
+			// MOUSE
 			Vector3 cameraDisplacement = (Input.mousePosition - lastMousePosition) * mouseRatio;
 			cameraX -= cameraDisplacement.y;
 			cameraY += cameraDisplacement.x;
 			cam.transform.rotation = Quaternion.Euler (new Vector3 (cameraX, cameraY, 0));
-		} else {
-			Cursor.lockState = CursorLockMode.None;
-			Cursor.visible = true;
-		}
-		if (withKeys) {
+			// KEYS
 			float yAxis = 0f;
 			if (Input.GetKey(KeyCode.E)) yAxis += 1;
 			if (Input.GetKey(KeyCode.Q)) yAxis -= 1;
 			body.velocity = cam.transform.rotation * new Vector3 (Input.GetAxis("Horizontal"), yAxis, Input.GetAxis("Vertical")) * speed;
+		} else {
+			// ARROW
+			golfBall.instance.arrow.SetActive(true);
+			// CURSOR
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+			// CAMERA POSITION BEHIND BALL
+			body.velocity = Vector3.zero;
+			if (!fireDirectionSet) {
+				golfBall.instance.transform.LookAt(golfBall.instance.hole.transform.position);
+				fireDirection = golfBall.instance.transform.rotation;
+				fireDirectionSet = true;
+			}
+			Vector3 angles = fireDirection.eulerAngles;
+			transform.rotation = Quaternion.Euler(new Vector3(angles.x, angles.y + Input.GetAxis("Horizontal"), angles.z));
+			golfBall.instance.arrow.transform.rotation = transform.rotation;
+			fireDirection = transform.rotation;
+			transform.position = golfBall.instance.transform.position + (fireDirection * new Vector3 (0,1,-2));
+			// KEYS
+			if (firePower != 0) {
+				if (Input.GetKeyDown(KeyCode.Space)) {
+					// FIXME FIRE
+					golfMode = false;
+					golfBall.instance.body.velocity = cam.transform.rotation * clubPower * Mathf.PingPong(firePower, 1);
+					firePower = 0f;
+				}
+				firePower += Time.deltaTime;
+			} else if (Input.GetKeyDown(KeyCode.Space)) {
+				firePower += Time.deltaTime;
+			}
+			powerMeter.offsetMax = new Vector2 (-290f + (Mathf.PingPong(firePower, 1) * 290f), powerMeter.offsetMax.y);
 		}
 		body.angularVelocity *= 0f;
 		lastMousePosition = Input.mousePosition;
---
+++ Assets/Scripts/golfBall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class golfBall : MonoBehaviour {
+
+	public static golfBall instance { get; private set; }
+	public Rigidbody body { get; private set; }
+	
+	public SphereCollider hole;
+	public GameObject arrow;
+
+	void Awake () {
+		if (!instance)
+			instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
